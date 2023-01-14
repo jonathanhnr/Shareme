02b0226e2518e002a395d8851980a11ff2846dfc
jonathan henrique <104086901+jonathanhnr@users.noreply.github.com>
2023-01-14 10:24:26 -0300

    fix router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.NODE_ENV === "production" ? "/Shareme" : "/"}>
       <App />
     </Router>
   </React.StrictMode>,
