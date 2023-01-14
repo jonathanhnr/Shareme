a16f28a2138804d202e2c7b3387fc9228360966a
jonathan henrique <104086901+jonathanhnr@users.noreply.github.com>
2023-01-14 10:51:41 -0300

    fix deploy again

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.NODE_ENV === "production" ? "/Shareme" : "/"}>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
